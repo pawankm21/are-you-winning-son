158e0ef3bf8fab19d06e1d65f1aedd5523ee4d91
Abhinav Narang <abhinavn2904@gmail.com>
2021-09-18 14:38:15 +0530

    added navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
-
-import './css/App.css';
-import StressSlider from './StressSlider';
+import { Fragment, useState } from "react";
+import "./css/App.css";
+import StressSlider from "./StressSlider";
+import NavBar from "./Navbar";
+import LoginForm from "./LoginForm";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return (
-   
-  <StressSlider></StressSlider>
-   
-  );
+  const [showLoginForm, setShowLoginForm] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false)
+  const loginClickHandler = () => {
+    setShowLoginForm(true);
+  }
+  const logoutClickHandler = () => {
+    setLoggedIn(false);
+  }
+  const closeForm = () => {
+    setShowLoginForm(false);
+  }
+  const loginHandler = () => {
+    setLoggedIn(true);
+    setShowLoginForm(false);
+  }
+	return (
+		<Fragment>
+      <header>
+        <NavBar onClickLogin={loginClickHandler} onClickLogout={logoutClickHandler} loggedIn={loggedIn} />
+      </header>
+      <StressSlider></StressSlider>
+      {showLoginForm && <LoginForm onClose={closeForm} onLogin={loginHandler}/>}
+		</Fragment>
+	);
 }
 
 export default App;
