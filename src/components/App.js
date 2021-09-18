e2b51c83d5ff55d0c7f59ad4dd0b1d3bbc2ea083
pawan <b419039@iiit-bh.ac.in>
2021-09-18 22:56:36 +0530

    added routing

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import Menu from './Menu';
 import NavBar from "./Navbar";
 import LoginForm from "./LoginForm";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
   const [showLoginForm, setShowLoginForm] = useState(false);
   const [loggedIn, setLoggedIn] = useState(false)
@@ -23,14 +23,29 @@ function App() {
     setShowLoginForm(false);
   }
   return (
-    <Fragment>
-      <header>
-        <NavBar onClickLogin={loginClickHandler} onClickLogout={logoutClickHandler} loggedIn={loggedIn} />
-      </header>
-      <StressSlider></StressSlider>
-      {showLoginForm && <LoginForm onClose={closeForm} onLogin={loginHandler} />}
-      <Menu />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <header>
+          <NavBar
+            onClickLogin={loginClickHandler}
+            onClickLogout={logoutClickHandler}
+            loggedIn={loggedIn}
+          />
+        </header>
+
+        {showLoginForm && (
+          <LoginForm onClose={closeForm} onLogin={loginHandler} />
+        )}
+      </Fragment>
+      <Switch>
+        <Route path="/">
+          <StressSlider></StressSlider>
+          <Menu />
+        </Route>
+        <Route path="/tips"></Route>
+        <Route path="/forum"></Route>
+      </Switch>
+    </Router>
   );
 }
 export default App;
