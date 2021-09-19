130e6a06123b9ef166ba72ce6029f94c12a71d4c
pawan <b419039@iiit-bh.ac.in>
2021-09-19 19:09:42 +0530

    final

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -3,28 +3,8 @@ import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import "./css/Timer.css";
 export default function Timer({ duration }) {
-  const [time, setTime] = useState(duration);
-  const [audio] = useState(new Audio("/sounds/timermusic.mp3"));
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTime(time - 1);
-      if (time <= 0) {
-        clearInterval(interval);
-        setTime(0);
-      }
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, [setTime, time]);
-  useEffect(() => {
-    if (time >= 0) {
-      audio.play();
-    }
-    audio.addEventListener("ended", () => audio.pause());
-    return () => {
-      audio.removeEventListener("ended",()=>{console.log("ended");});
-    };
-  });
+  const [time, setTime] = useState(10);
+  
   return (
     <div id="timer">
       <h1>Breath...</h1>
