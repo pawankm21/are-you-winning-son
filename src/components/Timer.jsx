48204b099a68c11936f61f4c3eca049a0813ff0a
pawan <b419039@iiit-bh.ac.in>
2021-09-19 19:10:18 +0530

    fixed error

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -4,7 +4,20 @@ import "react-circular-progressbar/dist/styles.css";
 import "./css/Timer.css";
 export default function Timer({ duration }) {
   const [time, setTime] = useState(10);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(time - 1);
+      if (time <= 0) {
+        clearInterval(interval);
   
+        setTime(0);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [setTime, time]);
+
   return (
     <div id="timer">
       <h1>Breath...</h1>
